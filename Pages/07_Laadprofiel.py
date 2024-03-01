9d7f1658baa4bb94d49cbd261a905e19a282bdd3
rickwol <r.wolbertus@hva.nl>
2024-03-01 09:52:28 +0100

    Update 07_Laadprofiel.py

--- Pages/07_Laadprofiel.py
+++ Pages/07_Laadprofiel.py
@@ -6,7 +6,16 @@ import datetime, timedelta
 from streamlit_extras.switch_page_button import switch_page
 
 st.set_page_config(page_title="Laadprofiel", page_icon="📈", initial_sidebar_state="collapsed")
-
+st.markdown(
+    """
+<style>
+    [data-testid="collapsedControl"] {
+        display: none
+    }
+</style>
+""",
+    unsafe_allow_html=True,
+)
 #st.sidebar.header("Ritprofielen")
 
 st.title("Heavy Duty Elektrificatie tool")
