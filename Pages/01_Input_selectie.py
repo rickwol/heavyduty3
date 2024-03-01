8b7f331ca5b082554c7ab84b7e5a652eda68af57
rickwol <r.wolbertus@hva.nl>
2024-03-01 09:51:52 +0100

    Update 01_Input_selectie.py

--- Pages/01_Input_selectie.py
+++ Pages/01_Input_selectie.py
@@ -6,7 +6,16 @@ import datetime, timedelta
 from streamlit_extras.switch_page_button import switch_page
 
 st.set_page_config(page_title="Voertuigen", page_icon="📈", initial_sidebar_state="collapsed")
-
+st.markdown(
+    """
+<style>
+    [data-testid="collapsedControl"] {
+        display: none
+    }
+</style>
+""",
+    unsafe_allow_html=True,
+)
 
 ###Set tooltips
 radio_markdown = '''
