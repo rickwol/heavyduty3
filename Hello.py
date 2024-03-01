1faf559c798a644fe4ec6dc0526f36cf3fad46b6
rickwol <r.wolbertus@hva.nl>
2024-03-01 09:01:32 +0100

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,45 @@
+import streamlit as st
+from st_pages import Page, add_page_title, show_pages
+
+st.set_page_config(page_title=None, page_icon=None, layout="centered", initial_sidebar_state="collapsed", menu_items=None)
+
+
+
+show_pages(
+    [
+        Page("Hello.py", "Home", "🏠"),
+        # Can use :<icon-name>: or the actual icon
+        Page("Pages/01_Input_selectie.py", "Input"),
+        #Page("Pages/01_Voertuig_selectie.py", "Voertuig"), 
+        Page("Pages/02_InAppInput.py", "In App Input"), 
+        #Page("Pages/02_Ritprofielen_Gemiddeld.py", "Ritprofieel Gemiddeld"),
+        Page("Pages/03_Ritprofielen_Meerdere.py", "Ritprofiel Gemiddeld"),
+        #Page("Pages/05_Laadprofiel_segment.py", "Ritprofiel Per Segment"),
+        Page("Pages/06_Resultaten.py", "Voertuig keuze"),
+        Page("Pages/09_ResultatenMeerdere.py", "Meerdere Voertuig keuze"),
+        Page("Pages/07_Laadprofiel.py", "Laadprofiel"),
+        Page("Pages/08_Netaansluiting.py", "Netaansluiting"),
+        #Page("Pages/06_Financieel.py", "Financieel overzicht"),
+
+       
+    ]
+)
+
+
+from streamlit_extras.switch_page_button import switch_page
+st.title("Welkom bij de Heavy Duty elektrificatie tool")
+
+st.markdown(
+    """
+    De tool voor een geïntegreerd advies voor elektrische logistiek. Op basis van ritgegevens krijgt u een advies over de keuze voor elektrische trucks en de bijbehorende laadinfrastructuur. 
+    
+    Aan de hand van ritprofielen van uw voertuigen krijgt u specifieker advies over de benodigde accucapaciteit van het voertuig en de laadcapaciteit van de infrastructuur. 
+    
+    Voor de ritprofielen heeft u keuze u voorgeselecteerde ritprofielen, handmatige input in deze app of gebruik te maken van uw eigen ritgegevens middels het format dat u ook aan het CBS moet aanleveren. 
+    
+    Door een beperkt aantal stappen te volgen en een keuze te maken over uw laadstrategie krijgt u direct beter inzicht in de mogelijkheden voor elektrificatie van uw wagenpark. 
+"""
+)
+
+if st.button("Volgende"):
+    switch_page("input")
