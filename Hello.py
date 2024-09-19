5dfaeec918d268f4984bc291742d95df5b7d1e25
rickwol <r.wolbertus@hva.nl>
2024-09-19 13:00:55 +0200

    Add files via upload

--- Hello.py
+++ Hello.py
@@ -58,4 +58,34 @@ st.markdown(
 
 if st.button("Start"):
     switch_page("input")
+
+###design footer
+footer="""<style>
+a:link , a:visited{
+color: blue;
+background-color: transparent;
+text-decoration: underline;
+}
+
+a:hover,  a:active {
+color: red;
+background-color: transparent;
+text-decoration: underline;
+}
+
+.footer {
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+background-color: white;
+color: black;
+text-align: center;
+}
+</style>
+<div class="footer">
+<img src="https://i.ibb.co/b6QF7F1/design.png">
+</div>
+"""
+st.markdown(footer,unsafe_allow_html=True)      
     
